6465ccf4cfb429aae6787d585ecd6cdaef15b944
808brick <ra2439@hawaii.edu>
2018-10-15 10:28:46 -1000

    Implemented new color thresholds into scan the code script

--- Projects/DeepLearning/ScanTheCode/color_recognition.py
+++ Projects/DeepLearning/ScanTheCode/color_recognition.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+print("\nEnsure roscore is running")
 import rospy
 import os
 import time
@@ -24,29 +25,56 @@ import cv2
 
 bridge = CvBridge()
 
-# lower_red = np.array([0,50,50])
-# upper_red = np.array([10,255,255])
+# lower_red = np.array([140,30,30])
+# upper_red = np.array([179,255,255])
 
-# lower_blue = np.array([115,50,50])
-# upper_blue = np.array([130,255,255])
+# lower_blue = np.array([108,60,60])
+# upper_blue = np.array([113,120,120])
 
+# lower_yellow = np.array([12,30,30])
+# upper_yellow = np.array([26,255,255])
 
-# lower_yellow = np.array([20,100,100])
-# upper_yellow = np.array([30,255,255])
+lower_red = np.array([140,120,60])
+upper_red = np.array([176,255,200])
 
-lower_red = np.array([140,30,30])
-upper_red = np.array([179,255,255])
-
-lower_blue = np.array([108,60,60])
-upper_blue = np.array([113,120,120])
+lower_blue = np.array([107,170,60])
+upper_blue = np.array([115,220,225])
 
 lower_yellow = np.array([12,30,30])
 upper_yellow = np.array([26,255,255])
 
-color_codes = {"red": (0,0,255), "blue": (255,0,0), "yellow": (0,255,255)}
+lower_green = np.array([74,30,30])
+upper_green = np.array([85,255,255])
+
+lower_gray = np.array([108,60,60])
+upper_gray = np.array([113,120,120])
+
+lower_threshold = {"red": lower_red,"blue": lower_blue, "yellow": lower_yellow, "green": lower_green, "gray": lower_gray}
+upper_threshold = {"red": upper_red,"blue": upper_blue, "yellow": upper_yellow, "green": upper_green, "gray": upper_gray}
+
+color_codes = {"red": (0,0,255), "blue": (255,0,0), "yellow": (0,255,255), "green": (0, 255, 0), "gray": (192,192,192)}
 
 classified_colors_list = []
 times = []
+
+def intro():
+	image_topic = raw_input("\n\nName of the topic which the Images are Published (ex: topic_name/sub_topic): \n")
+	image_type = raw_input("\n\nWill you be subscribing to images of message type Image (i) or CompressedImage (c)  (i/c):\n")
+	if image_type == 'c': image_type = 'CompressedImage'
+	else: image_type = 'Image'
+	print(chr(27) + "[2J")
+	print "Images will be subscribed from topic ", image_topic, "\nwith message type: ", image_type, "\n\n... Is this correct? (y/n)"
+	confirmation = raw_input("")
+
+	if confirmation != "y":
+		print("Please Restart the script and try entering the information correctly")
+		assert confirmation == "y", "image topic and number not properly specified"
+
+	print(chr(27) + "[2J")
+			
+	print "\n\nA new window will open displaying the images from topic:", image_topic, "\n\n"
+
+	return image_topic, image_type
 
 def scan_the_code(colors, times):
     diff = np.asarray([])
@@ -78,27 +106,18 @@ def plot_scan_the_code(scanned):
 	cv2.putText(scan_the_code_report_template, scanned[1], (450,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2);
 	cv2.putText(scan_the_code_report_template, scanned[2], (780,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2);
 
-	# cv2.putText(scan_the_code_report_template, (60, 150), scanned[0], fontScale = 5, fontFace = 1, color=(255,255,255))
-	# cv2.putText(scan_the_code_report_template, (380, 150), scanned[0], fontScale = 5, fontFace = 1, color=(255,255,255))
-	# cv2.putText(scan_the_code_report_template, (695, 150), scanned[0], fontScale = 5, fontFace = 1, color=(255,255,255))
-
 	cv2.namedWindow('Scan the Code',cv2.WINDOW_NORMAL)
 	cv2.resizeWindow('Scan the Code', 700,500)
 	cv2.imshow("Scan the Code", scan_the_code_report_template)
 	cv2.waitKey(1)
 
-	# ax.add_patch(a)
-	# ax.add_patch(b)
-	# ax.add_patch(c)
-	        
-	# plt.show()
-
-
 def color_recognition(image):
     classified, coordinates, radius_dict, mask_dict, res_dict = {}, {}, {}, {}, {}
     
-    lower_threshold = {"red": lower_red,"blue": lower_blue, "yellow": lower_yellow}
-    upper_threshold = {"red": upper_red,"blue": upper_blue, "yellow": upper_yellow}
+    # lower_threshold = {"red": lower_red,"blue": lower_blue, "yellow": lower_yellow}
+    # upper_threshold = {"red": upper_red,"blue": upper_blue, "yellow": upper_yellow}
+    global lower_threshold
+    global upper_threshold
     
     for color in lower_threshold.keys():
         lower_color = lower_threshold[color]
@@ -133,15 +152,26 @@ def color_recognition(image):
 
 class image_converter:
 
+  global image_topic
+  global image_type
   def __init__(self):
     # self.image_pub = rospy.Publisher("image_topic_2",Image)
     print("Initializing Class")
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("camera1/usb_cam/image_raw/compressed",CompressedImage,self.callback, queue_size=1)
+    # self.image_sub = rospy.Subscriber("camera1/usb_cam/image_raw/compressed",CompressedImage,self.callback, queue_size=1)
+    if image_type == "Image":
+	    self.image_sub = rospy.Subscriber(image_topic,Image,self.callback, queue_size=1)
+    else:
+	    self.image_sub = rospy.Subscriber(image_topic,CompressedImage,self.callback, queue_size=1)
 
   def callback(self,data):
+  	global image_type
 	try:
-	  cv_image = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+
+	  if image_type == "Image": 
+		  cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+	  else:	
+		  cv_image = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
 	  cropped_image = cv_image[100:200, 50:600]
 	  cv_image_hsv = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2HSV)
 	except CvBridgeError as e:
@@ -193,8 +223,8 @@ class image_converter:
 			elif color == 'blue':
 				cv2.circle(cv_image, (x+50, y+100), r, (255))
 
-			elif color == 'yellow':
-				cv2.circle(cv_image, (x+50, y+100), r, (0,255,255))
+			elif color == 'green':
+				cv2.circle(cv_image, (x+50, y+100), r, (0,255,0))
 
 			else:
 				cv2.circle(cv_image, (x+50, y+100), r, (0,0,0))
@@ -212,13 +242,19 @@ class image_converter:
     #   print(e)
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+
+	global image_topic
+	global image_type
+
+	image_topic, image_type = intro()
+
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+		cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
