9c6923525294a022ff09787ef8841e00b1d4f397
brennanyama <brennanyama@gmail.com>
2019-03-05 18:03:13 -1000

    Added noise rejection algorithm to wamv mainArduino and wamv killArduino

---
+++ SurfaceVehicles/WAMV/Arduino/MainMega/a20190305_wamvMainArduino/a20190305_wamvMainArduino.ino
@@ -0,0 +1,235 @@
+/* 
+ *  All libraries available from Library Manager built into Arduino IDE.
+ *  To access go to Sketch > Include Library > Manage Libraries
+ *    ROS: Search for "Rosserial Arduino Library by Michael Ferguson" and install.
+ *    Adafruit: Search for "Adafruit PWM Servo Driver Library by Adafruit" and install.
+ *    QuickMedian: Search for "QuickMedianLib by Luis Llamas" and install
+ *    
+ *  Created by: Team Kanaloa (http://rip.eng.hawaii.edu/research/unmanned-x-systems/)
+ *  Last update: 2019.03.05
+ *     
+ */
+
+// Library inclusions
+#include <Arduino.h>                    // Arduino inclusions
+#include <ros.h>                        // ROS inclusions
+#include <std_msgs/Bool.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Float64.h>
+#include "QuickMedianLib.h"             // Quick median libarary inclusions
+#include "Adafruit_PWMServoDriver.h"    // Adafruit servo shield inclusions
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Pin definitions
+const int voltMainPin = A1;         // analog in from voltage divider pin for reading main battery voltage
+const int tempPin = A2;             // analog in from temperature probe pin for reading temperature (optional)
+const byte ch1Pin = 18;             // PWM in from remote control receiver channel 1 (right stick left-to-right yaw/rotation)
+const byte ch4Pin = 19;             // PWM in from remote control receiver channel 4 (left stick left-to-right sway/strafe)
+const byte ch6Pin = 3;              // PWM in from remote control receiver channel 6 (left stick up-to-down surge/forward) 
+const byte modeCommPin = 11;        // digital in auto/manual mode state from kill arduino 
+const byte revConKillCommPin = 12;  // digital in reverse contactor kill state from kill arduino 
+const byte killCommPin = 13;        // digital in kill state from kill arduino 
+const byte servoQ1Pin = 4;          // q1 channel on servo shield (note: not the same as a digital pin!)
+const byte servoQ2Pin = 5;          // q2 channel on servo shield (note: not the same as a digital pin!)
+const byte servoQ3Pin = 6;          // q3 channel on servo shield (note: not the same as a digital pin!)
+const byte servoQ4Pin = 7;          // q4 channel on servo shield (note: not the same as a digital pin!)
+const byte revConKillPin = 30;      // digital out to reversing contactor (HIGH for kill, LOW for unkill)
+const byte revConQ1Pin = 22;        // digital out to Q1 reversing contactor (HIGH for motor to spin forward, LOW to spin in reverse)
+const byte revConQ2Pin = 24;        // digital out to Q2 reversing contactor (HIGH for motor to spin forward, LOW to spin in reverse)
+const byte revConQ3Pin = 26;        // digital out to Q3 reversing contactor (HIGH for motor to spin forward, LOW to spin in reverse)
+const byte revConQ4Pin = 28;        // digital out to Q4 reversing contactor (HIGH for motor to spin forward, LOW to spin in reverse)
+
+// Global system (constant) variables
+const int nMedian = 8;            // number of readings to take to calculate median from controller inputs
+const float receiverRR = 50;      // refresh rate of the receiver [Hz] (MUST BE A FLOAT!)
+const int ch1PulseMin = 1176;     // minimum expected pulse width of ch1 [us]
+const int ch1PulseMax = 2176;     // maximum expected pulse width of ch1 [us]
+const int ch4PulseMin = 1176;     // minimum expected pulse width of ch4 [us]
+const int ch4PulseMax = 2176;     // maximum expected pulse width of ch4 [us]
+const int ch6PulseMin = 1240;     // minimum expected pulse width of ch6 [us]
+const int ch6PulseMax = 2240;     // maximum expected pulse width of ch6 [us]
+const int pulseTolerance = 200;   // tolerance range on minimum and maximum expected pulse widths [us]
+const int pulseDeadzone = 125;    // deadzone range at the center of each joystick [us]
+const int accLimit = 80;          // acceleration limit (maximum change in thrust percentage per second) [%/s]
+
+// Global volatile variables
+volatile int ch1PulseRaw = 0;     // channel 1 pulse width (for interrerupt service routine)
+volatile int ch1Timer = 0;        // channel 1 pulse width timer (for interrerupt service routine)
+volatile int ch4PulseRaw = 0;     // channel 4 pulse width (for interrerupt service routine)
+volatile int ch4Timer = 0;        // channel 4 pulse width timer (for interrerupt service routine)
+volatile int ch6PulseRaw = 0;     // channel 6 pulse width (for interrerupt service routine)
+volatile int ch6Timer = 0;        // channel 6 pulse width timer (for interrerupt service routine)
+
+// Global code variables
+int mode;                   // determines manual (1), autonomous (2), or invalid (0) operation mode
+float voltMain;             // voltage from the main battery
+int killStatus = 1;         // status of the kill switch from kill Arduino
+int revConKillStatus = 1;   // status of reverse contactor kill switch from kill Arduino
+int modeStatus = 1;         // auto/manual status from kill Arduino
+int ch1Filtered = 0;        // filtered output from ch1
+int ch4Filtered = 0;        // filtered output from ch4
+int ch6Filtered = 0;        // filtered output from ch6
+int q1Setpoint = 0;         // Q1 setpoint from -1000 to 1000 (no acceleration limit)
+int q2Setpoint = 0;         // Q2 setpoint from -1000 to 1000 (no acceleration limit)
+int q3Setpoint = 0;         // Q3 setpoint from -1000 to 1000 (no acceleration limit)
+int q4Setpoint = 0;         // Q4 setpoint from -1000 to 1000 (no acceleration limit)
+float timeNow = 0;          // time of current iteration (for acceleration limit calculations)
+float timeLast = 0;         // time of last iteration (for acceleration limit calculations)
+int q1Out = 0;              // Q1 output from -1000 to 1000 (with acceleration limits)
+int q2Out = 0;              // Q2 output from -1000 to 1000 (with acceleration limits)
+int q3Out = 0;              // Q3 output from -1000 to 1000 (with acceleration limits)
+int q4Out = 0;              // Q4 output from -1000 to 1000 (with acceleration limits)
+int q1Last = 0;             // Q1 output for last iteration (for acceleration limit calculations)
+int q2Last = 0;             // Q2 output for last iteration (for acceleration limit calculations)
+int q3Last = 0;             // Q3 output for last iteration (for acceleration limit calculations)
+int q4Last = 0;             // Q4 output for last iteration (for acceleration limit calculations)
+int q1Dir = 0;              // Q1 direction (0 = forward, 1 = reverse)
+int q2Dir = 0;              // Q2 direction (0 = forward, 1 = reverse)
+int q3Dir = 0;              // Q3 direction (0 = forward, 1 = reverse)
+int q4Dir = 0;              // Q4 direction (0 = forward, 1 = reverse)
+float autoQ1 = 0;           // autonomous command signal from ROS for Q1 thruster (via ROS)
+float autoQ2 = 0;           // autonomous command signal from ROS for Q2 thruster (via ROS)
+float autoQ3 = 0;           // autonomous command signal from ROS for Q3 thruster (via ROS)
+float autoQ4 = 0;           // autonomous command signal from ROS for Q4 thruster (via ROS)
+
+// ROS node handle (allows program to create publishers and subscribers)
+ros::NodeHandle nh;         // ROS node handle (allows program to create publishers and subscribers)
+
+// ROS messages
+std_msgs::UInt16 q1Msg;         // create Q1 thruster message
+std_msgs::UInt16 q2Msg;         // create Q2 thruster message
+std_msgs::UInt16 q3Msg;         // create Q3 thruster message
+std_msgs::UInt16 q4Msg;         // create Q4 thruster message
+std_msgs::Float64 voltMainMsg;  // create main battery voltage message
+
+// ROS publishers
+ros::Publisher q1Pub("q1", &q1Msg);                   // Q1 thruster publisher
+ros::Publisher q2Pub("q2", &q2Msg);                   // Q2 thruster publisher
+ros::Publisher q3Pub("q3", &q3Msg);                   // Q3 thruster publsiher
+ros::Publisher q4Pub("q4", &q4Msg);                   // Q4 thruster publisher
+ros::Publisher voltMainPub("voltMain", &voltMainMsg); // main battery voltage publisher
+
+// ROS Subscribers
+void autoQ1Cb(const std_msgs::Float64& autoQ1CbMsg) {
+  autoQ1 = autoQ1CbMsg.data;
+}
+ros::Subscriber<std_msgs::Float64> autoQ1sub("autoQ1", &autoQ1Cb);   // autonomous Q1 thruster output
+void autoQ2Cb(const std_msgs::Float64& autoQ2CbMsg) {
+  autoQ2 = autoQ2CbMsg.data;
+}
+ros::Subscriber<std_msgs::Float64> autoQ2sub("autoQ2", &autoQ2Cb);   // autonomous Q2 thruster output
+void autoQ3Cb(const std_msgs::Float64& autoQ3CbMsg) {
+  autoQ3 = autoQ3CbMsg.data;
+}
+ros::Subscriber<std_msgs::Float64> autoQ3sub("autoQ3", &autoQ3Cb);   // autonomous Q3 thruster output
+void autoQ4Cb(const std_msgs::Float64& autoQ4CbMsg) {
+  autoQ4 = autoQ4CbMsg.data;
+}
+ros::Subscriber<std_msgs::Float64> autoQ4sub("autoQ4", &autoQ4Cb);   // autonomous Q4 thruster output
+
+void setup() {
+  
+  // Initialize ROS node handle
+  nh.initNode();
+
+  // Initialize ROS publishers
+  nh.advertise(voltMainPub);    // main battery voltage
+  nh.advertise(q1Pub);          // Q1 thruster output
+  nh.advertise(q2Pub);          // Q2 thruster output
+  nh.advertise(q3Pub);          // Q3 thruster output
+  nh.advertise(q4Pub);          // Q4 thruster output
+
+  // Initialize ROS subscibers
+  nh.subscribe(autoQ1sub);      // autonomous Q1 thruster output
+  nh.subscribe(autoQ2sub);      // autonomous Q2 thruster output
+  nh.subscribe(autoQ3sub);      // autonomous Q3 thruster output
+  nh.subscribe(autoQ4sub);      // autonomous Q4 thruster output
+
+  // Set pin mode
+  pinMode(voltMainPin,INPUT);
+  pinMode(tempPin,INPUT);
+  pinMode(ch1Pin,INPUT);
+  pinMode(ch4Pin,INPUT);
+  pinMode(ch6Pin,INPUT);
+  pinMode(modeCommPin,INPUT_PULLUP);
+  pinMode(revConKillCommPin,INPUT_PULLUP);
+  pinMode(killCommPin,INPUT_PULLUP);
+  pinMode(revConKillPin,OUTPUT);
+  pinMode(revConQ1Pin,OUTPUT);
+  pinMode(revConQ2Pin,OUTPUT);
+  pinMode(revConQ3Pin,OUTPUT);
+  pinMode(revConQ4Pin,OUTPUT);
+
+  // Attach interrupts
+  attachInterrupt(digitalPinToInterrupt(ch1Pin),ch1Rise,RISING);
+  attachInterrupt(digitalPinToInterrupt(ch1Pin),ch1Fall,FALLING);
+  attachInterrupt(digitalPinToInterrupt(ch4Pin),ch4Rise,RISING);
+  attachInterrupt(digitalPinToInterrupt(ch4Pin),ch4Fall,FALLING);
+  attachInterrupt(digitalPinToInterrupt(ch6Pin),ch6Rise,RISING);
+  attachInterrupt(digitalPinToInterrupt(ch6Pin),ch6Fall,FALLING);
+  
+  //Initialize PWM shield
+  pwm.begin();
+  pwm.setPWMFreq(500);
+  
+  // Set serial baud rate
+  Serial.begin(57600); 
+  
+}
+
+void loop() {
+  
+  readKillArduino();      // read kill state from kill arduino
+
+  if (killStatus == 0) {
+
+    // ===== AUTONOMOUS CODE GOES HERE =====
+    if (modeStatus == 0) {
+
+      Serial.println("AUTONOMOUS MODE: One day, I shall achieve autonomy! OM NOM NOM NOM!");
+      delay(5);
+      
+    }
+
+    // ===== MANUAL CODE GOES HERE =====
+    else {
+
+      // Filter noise from remote control joystick inputs using meadian of multiple measurements
+      filterJoy();
+
+      // Take joystick readings, and convert to setpoint thrust values
+      joy2Setpoint();
+
+      // Calculate acceleration-limited outputs as a function of the setpoint and send to thrusters
+      setpoint2Output();
+
+      // Calculate reverse contactor states and send to reverse contactors
+      output2RevCon();
+
+      // Short delay to prevent errors
+      delay(5);
+
+      // Print debug info
+      Serial.print("MANUAL MODE: Q1 (");
+      Serial.print(q1Out);
+      Serial.print(")  Q2 (");
+      Serial.print(q2Out);
+      Serial.print(")  Q3 (");
+      Serial.print(q3Out);
+      Serial.print(")  Q4 (");
+      Serial.print(q4Out);
+      Serial.println(")");
+      
+    }
+
+  }
+  else {
+    
+    Serial.println("System is killed, waiting for unkill command...");
+
+    // Short delay to prevent errors
+    delay(5);
+    
+  }
+
+}
---
+++ SurfaceVehicles/WAMV/Arduino/MainMega/a20190305_wamvMainArduino/io.ino
@@ -0,0 +1,250 @@
+// Read in information from the kill Arduino, and assign mode of operation accordingly.  Note that the mainMega cannot actually kill the system, since the actual kill is handled by the killMega. 
+void readKillArduino() {
+
+  if (digitalRead(killCommPin) == LOW) {
+    killStatus = 0;       // killMega sending LOW (UNKILL)
+  }
+  else {
+    killStatus = 1;       // killMega sending HIGH (KILL)
+  }
+  if (digitalRead(revConKillCommPin) == LOW) {
+    revConKillStatus = 0; // killMega reverse contactor sending LOW (UNKILL)
+  }
+  else {
+    revConKillStatus = 1; // killMega reverse contactor sending HIGH (KILL)
+  }
+  if (digitalRead(modeCommPin) == LOW){
+    modeStatus = 0;       // killMega auto/manual mode sending LOW (autonomous)
+  }
+  else {
+    modeStatus = 1;       // killMega auto/manual mode sending HIGH (manual)
+  }
+  
+}
+
+// Filter noise from remote control joystick inputs using meadian of multiple measurements
+void filterJoy() {
+
+  // Preallocate arrays
+  int ch1PulseArray[nMedian];
+  int ch4PulseArray[nMedian];
+  int ch6PulseArray[nMedian];
+
+  // Calculate delay for each reading based on refresh rate of receiver
+  int delayTime = (round((1/receiverRR)*1000));  // [ms]
+
+  // Loop, taking a measurement each time and saving to array
+  for (int i = 0; i <= nMedian - 1; i++) {
+
+    // Take a measurement from the interrupt global variable
+    ch1PulseArray[i] = ch1PulseRaw;   // yaw (right stick left-right)
+    ch4PulseArray[i] = ch4PulseRaw;   // sway (left stick left-right)
+    ch6PulseArray[i] = ch6PulseRaw;   // surge (left stick up-down)
+
+    // Wait for receiver to push another value
+    delay(delayTime);
+
+  }
+
+  // Save the median of each measurement array to global variable
+  ch1Filtered = QuickMedian<int>::GetMedian(ch1PulseArray,nMedian);
+  ch4Filtered = QuickMedian<int>::GetMedian(ch4PulseArray,nMedian);
+  ch6Filtered = QuickMedian<int>::GetMedian(ch6PulseArray,nMedian);
+
+//  Serial.print("CH 1 (");
+//  Serial.print(ch1Filtered);
+//  Serial.print(")  CH 4 (");
+//  Serial.print(ch4Filtered);
+//  Serial.print(")  CH 6 (");
+//  Serial.print(ch6Filtered);
+//  Serial.println(")");
+
+}
+
+// Take joystick readings, and convert to setpoint thrust values
+void joy2Setpoint() {
+
+  // Grab ch1, ch4, and ch6 from filtering function
+  int ch1Pulse = ch1Filtered;   // yaw (right stick left-right)
+  int ch4Pulse = ch4Filtered;   // sway (left stick left-right)
+  int ch6Pulse = ch6Filtered;   // surge (left stick up-down)
+
+  // Pulse width variables
+  int ch1PulseNeutral = round((ch1PulseMax - ch1PulseMin) / 2) + ch1PulseMin;
+  int ch4PulseNeutral = round((ch4PulseMax - ch4PulseMin) / 2) + ch4PulseMin;
+  int ch6PulseNeutral = round((ch6PulseMax - ch6PulseMin) / 2) + ch6PulseMin;
+
+  // Remove the deadzone
+  if (ch1Pulse < ch1PulseNeutral + round(pulseDeadzone/2) && ch1Pulse > ch1PulseNeutral - round(pulseDeadzone/2)) {
+    ch1Pulse = ch1PulseNeutral;
+  }
+  if (ch4Pulse < ch4PulseNeutral + round(pulseDeadzone/2) && ch4Pulse > ch4PulseNeutral - round(pulseDeadzone/2)) {
+    ch4Pulse = ch4PulseNeutral;
+  }
+  if (ch6Pulse < ch6PulseNeutral + round(pulseDeadzone/2) && ch6Pulse > ch6PulseNeutral - round(pulseDeadzone/2)) {
+    ch6Pulse = ch6PulseNeutral;
+  }
+
+  // Map joystick inputs from -1000 to 1000
+  int ch1Map = map(ch1Pulse,ch1PulseMin,ch1PulseMax,-1000,1000);
+  int ch4Map = map(ch4Pulse,ch4PulseMin,ch4PulseMax,-1000,1000);
+  int ch6Map = map(ch6Pulse,ch6PulseMin,ch6PulseMax,-1000,1000);
+
+  // Calculate surge sway and yaw components
+  int surgeQ1 = ch6Map;   // surge (forward positive)
+  int surgeQ2 = ch6Map;
+  int surgeQ3 = ch6Map;
+  int surgeQ4 = ch6Map;
+  int swayQ1 = -ch4Map;   // sway (right positive)
+  int swayQ2 = ch4Map;
+  int swayQ3 = -ch4Map;
+  int swayQ4 = ch4Map;
+  int yawQ1 = -ch1Map;    // yaw (CLOCKWISE positive)
+  int yawQ2 = ch1Map;
+  int yawQ3 = ch1Map;
+  int yawQ4 = -ch1Map;
+
+  // Map thruster components from -1000 to 1000
+  q1Setpoint = constrain(surgeQ1 + swayQ1 + yawQ1,-1000,1000);
+  q2Setpoint = constrain(surgeQ2 + swayQ2 + yawQ2,-1000,1000);
+  q3Setpoint = constrain(surgeQ3 + swayQ3 + yawQ3,-1000,1000);
+  q4Setpoint = constrain(surgeQ4 + swayQ4 + yawQ4,-1000,1000);
+
+//  Serial.print("Q1: ");
+//  Serial.print(q1Setpoint);
+//  Serial.print("   Q2: ");
+//  Serial.print(q2Setpoint);
+//  Serial.print("   Q3: ");
+//  Serial.print(q3Setpoint);
+//  Serial.print("   Q4: ");
+//  Serial.println(q4Setpoint);
+  
+}
+
+// Calculate acceleration-limited outputs as a function of the setpoint and send to thrusters
+void setpoint2Output() {
+
+  // Calculate time delta since last loop
+  timeNow = millis();
+  float timeDelta = (timeNow - timeLast) * 0.001;
+
+  // Calculate maximum thrust delta
+  float thrustDelta = accLimit * 10 * timeDelta;  // maximum change in thrust component percentage in this loop [%]
+
+  // Calculate actual thrust delta
+  int q1Delta = q1Setpoint - q1Last;
+  int q2Delta = q2Setpoint - q2Last;
+  int q3Delta = q3Setpoint - q3Last;
+  int q4Delta = q4Setpoint - q4Last;
+  
+  // Limit Q1 output thrust if change in setpoint is greater than allowable delta 
+  if (abs(q1Delta) > thrustDelta) {
+    if (q1Delta > 0) {
+    q1Out = q1Last + thrustDelta;
+    }
+    else {
+    q1Out = q1Last - thrustDelta;
+    }
+  }
+  else {
+    q1Out = q1Setpoint;
+  }
+
+  // Limit Q2 output thrust if change in setpoint is greater than allowable delta 
+  if (abs(q2Delta) > thrustDelta) {
+    if (q2Delta > 0) {
+    q2Out = q2Last + thrustDelta;
+    }
+    else {
+    q2Out = q2Last - thrustDelta;
+    }
+  }
+  else {
+    q2Out = q2Setpoint;
+  }
+
+  // Limit Q3 output thrust if change in setpoint is greater than allowable delta 
+  if (abs(q3Delta) > thrustDelta) {
+    if (q3Delta > 0) {
+    q3Out = q3Last + thrustDelta;
+    }
+    else {
+    q3Out = q3Last - thrustDelta;
+    }
+  }
+  else {
+    q3Out = q3Setpoint;
+  }
+
+  // Limit Q4 output thrust if change in setpoint is greater than allowable delta 
+  if (abs(q4Delta) > thrustDelta) {
+    if (q4Delta > 0) {
+    q4Out = q4Last + thrustDelta;
+    }
+    else {
+    q4Out = q4Last - thrustDelta;
+    }
+  }
+  else {
+    q4Out = q4Setpoint;
+  }
+
+  // Save timeNow and thruster output components for calculation in next loop
+  timeLast = timeNow;
+  q1Last = q1Out;
+  q2Last = q2Out;
+  q3Last = q3Out;
+  q4Last = q4Out;
+  
+  // Publish to ROS
+  q1Msg.data = q1Out;
+  q2Msg.data = q2Out;
+  q3Msg.data = q3Out;
+  q4Msg.data = q4Out;
+  q1Pub.publish(&q1Msg);
+  q2Pub.publish(&q2Msg);
+  q3Pub.publish(&q3Msg);
+  q4Pub.publish(&q4Msg);
+
+  // Output to thrusters
+  const float servoRes = 4095;
+  float q1ServoOut = abs(q1Out * servoRes / 100);   // calculate the bit output to Q1 DAC
+  pwm.setPWM(servoQ1Pin,0,q1ServoOut);              // set q1 bit command to servo shield
+  float q2ServoOut = abs(q2Out * servoRes / 100);   // calculate the bit output to Q2 DAC
+  pwm.setPWM(servoQ2Pin,0,q2ServoOut);              // set q2 bit command to servo shield
+  float q3ServoOut = abs(q3Out * servoRes / 100);   // calculate the bit output to Q3 DAC
+  pwm.setPWM(servoQ3Pin,0,q3ServoOut);              // set q3 bit command to servo shield
+  float q4ServoOut = abs(q4Out * servoRes / 100);   // calculate the bit output to Q4 DAC
+  pwm.setPWM(servoQ4Pin,0,q4ServoOut);              // set q4 bit command to servo shield
+  
+}
+
+// Calculate reverse contactor states and send to reverse contactors
+void output2RevCon() {
+  
+  // Control direction of reversing contactor
+  if (q1Out >= 0) {
+    digitalWrite(revConQ1Pin,LOW);
+  }
+  else {
+    digitalWrite(revConQ1Pin,HIGH);
+  } 
+  if (q2Out >= 0) {
+    digitalWrite(revConQ2Pin,LOW);
+  }
+  else {
+    digitalWrite(revConQ2Pin,HIGH);
+  }
+  if (q3Out >= 0) {
+    digitalWrite(revConQ3Pin,LOW);
+  }
+  else {
+    digitalWrite(revConQ3Pin,HIGH);
+  }
+  if (q4Out >= 0) {
+    digitalWrite(revConQ4Pin,LOW);
+  }
+  else {
+    digitalWrite(revConQ4Pin,HIGH);
+  }  
+}
