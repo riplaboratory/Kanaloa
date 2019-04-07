c801be61fd520ee4e794a9b09493e1ba81a53cd7
kaijones21 <kaijones@hawaii.edu>
2019-04-06 19:07:04 -1000

    Add files via upload

---
+++ SurfaceVehicles/WAMV/Arduino/VoltageReader/a20190406_voltageReader.ino
@@ -0,0 +1,16 @@
+int voltagePin = A0; 
+float voltageBytes; 
+float voltage;
+
+void setup() {
+  pinMode(voltagePin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  voltageBytes = analogRead(voltagePin);
+  voltage = 0.0427*voltageBytes + 0.117; 
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  delay(100); 
+}
