ca0b4a310fb01c62e2d95149aebaa58c6bc33129
Raymond Andrade <raymond808state1@gmail.com>
2019-04-30 19:41:32 -1000

    Deleted other brick

--- Projects/Brick/public/Plugins/ros_image_viewer_terminal/run_python_script.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-var yourscript = exec('sh hi.sh',
-        (error, stdout, stderr) => {
-            // console.log(`${stdout}`);
-            // console.log(`${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
