be9bcce2ae3cd4de5eb0383c31f98ea397c71259
Brennan <brennanyama@gmail.com>
2019-09-15 14:27:29 -1000

    minor changes for raymond

--- SurfaceVehicles/WAMV/Arduino/MainMega/a20190915_wamvMainArduino/outputs.ino
+++ SurfaceVehicles/WAMV/Arduino/MainMega/a20190915_wamvMainArduino/outputs.ino
@@ -388,6 +388,57 @@ void changeLight(int lightColor) {
   }
 }
 
+void joy2Setpoint() {
+  /*
+     Take readings fromt receiver joystick channels, and convert to setpoint thruster values from -1000 to 1000.
+  */
+
+  // Grab ch1, ch4, and ch6 from filtering function
+  int ch1Pulse = ch1PulseMedian;    // yaw (right stick left-right)
+  int ch2Pulse = ch2PulseMedian;    // sway (left stick left-right)
+
+  // Pulse width variables
+  int ch1PulseNeutral = round((ch1PulseMax - ch1PulseMin) / 2) + ch1PulseMin;
+  int ch2PulseNeutral = round((ch2PulseMax - ch2PulseMin) / 2) + ch2PulseMin;
+
+  // Remove the deadzone
+  if (ch1Pulse < ch1PulseNeutral + round(pulseDeadzone / 2) && ch1Pulse > ch1PulseNeutral - round(pulseDeadzone / 2)) {
+    ch1Pulse = ch1PulseNeutral;
+  }
+  if (ch2Pulse < ch2PulseNeutral + round(pulseDeadzone / 2) && ch2Pulse > ch2PulseNeutral - round(pulseDeadzone / 2)) {
+    ch2Pulse = ch2PulseNeutral;
+  }
+
+  // Map joystick inputs from -1000 to 1000
+  int ch1Map = map(ch1Pulse, ch1PulseMin, ch1PulseMax, -1000, 1000); // surge (positive forward, negative backward)
+  int ch2Map = map(ch2Pulse, ch2PulseMin, ch2PulseMax, 1000, -1000); // yaw (positive CCW, negative CW)
+
+  // Calculate surge sway and yaw components
+  int surgeLeft = ch1Map;     // surge (forward positive)
+  int surgeRight = ch1Map;
+  int yawLeft = -ch2Map;       // yaw (CCW positive)
+  int yawRight = ch2Map;
+
+  // Map thruster components from -1000 to 1000
+  leftThrusterSetpoint = constrain(surgeLeft + yawLeft, -1000, 1000);
+  rightThrusterSetpoint = constrain(surgeRight + yawRight, -1000, 1000);
+
+}
+
+void zeroSetpoints() {
+  /*
+     Sets the thruster setpoints to zero.  This should be used when the system is killed to prevent the high current arduino controller from going crazy while system is killed.
+  */
+
+  // Set thruster components to zero
+  leftThrusterSetpoint = 0;
+  rightThrusterSetpoint = 0;
+
+  // Create messages for I2C comms
+  createI2cMsg();
+
+}
+
 void createI2cMsg() {
   /*
      Create message to send over I2C (based on the thruster setpoints)
