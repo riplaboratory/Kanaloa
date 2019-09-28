dcdc6fc5843a9eb901b8a9b1621b72ed126e5382
A Trimble <atrimble@hawaii.edu>
2019-09-28 09:42:32 -1000

    Improve commenting and documentation of kanaloaMotor Class

--- SurfaceVehicles/WAMV/Arduino/Libraries/KanaloaMotorClass/kanaloaMotor.h
+++ SurfaceVehicles/WAMV/Arduino/Libraries/KanaloaMotorClass/kanaloaMotor.h
@@ -2,7 +2,18 @@
  *  Created by: Team Kanaloa (http://rip.eng.hawaii.edu/research/unmanned-x-systems/)
  *  LICENSE: Internal Kanaloa use only
  *  
- *  Learn how to control a mtor output voltage
+ *  PID feed back control of MinnKota Motor controllers output voltage
+ *  # Hardware - hardware the code is designed to control
+ *      - PWM Servo shield
+ *      - 2.5 V constant ouput control board. Requires 2 channels one to 
+ *          provide forward commands and one to provide reverse commands
+ *      - ADS1115 ADC circuit
+ *  # Libraries
+ *      ## Library Manager (Sketch ->Include Library -> Manage Libraries)
+ *          - "Adafruit PWM Servo Driver Library" by Adafruit
+ *          - "Adafruit ADS1X15" by Adafruit
+ *      ## Downloaded
+ *          - PID_v1 by Brett Beauregard, ADD GITHUB WEBSITE
  *  
  *  # Version History
  *    2019.09.13 - A Trimble (atrimble@hawaii.edu)
@@ -12,7 +23,6 @@
 #ifndef kanaloaMotor_h
 #define kanaloaMotor_h
 
-//#include <Adafruit_ADS1015.h>
 #include <PID_v1.h>
 
 #define PWM_SPD_MAX 4095
@@ -21,12 +31,16 @@
 #define CALIBRATION_CONSTANT 0.003768 //20.1*0.1875/1000
 #define VMAX 24.0
 #define QMAX 1000.0
+#define DEAD_ZONE_LIMIT 2 
 
 
 class kanaloaMotor {  
   public:
     // Constructors
-    kanaloaMotor(Adafruit_PWMServoDriver& pwm, byte forwardPin, byte reversePin, byte color);
+    kanaloaMotor(Adafruit_PWMServoDriver& pwm, 
+    byte forwardPin,
+    byte reversePin,
+    byte color);
 
     // Functions
     void init();
@@ -36,6 +50,7 @@ class kanaloaMotor {
     void setVoltageSetpointFromQcommand(int qin);
     double getMotorVoltage();
     double getVoltageSetpoint() const;
+    void debugOutput();
     void printGains();
     void printPWMcommands();
 
